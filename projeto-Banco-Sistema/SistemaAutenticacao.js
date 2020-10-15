b65ffa3fc9cf0932f9488cd9c8c734dc9cfa623e
mateus_Wagnitz <70395226+MateusWagnitz@users.noreply.github.com>
2020-10-15 14:09:25 -0300

    Versão final
    Sistema básico de banco, criado com JS puro, um estudo sobre orientação de objeto.

---
+++ projeto-Banco-Sistema/SistemaAutenticacao.js
@@ -0,0 +1,14 @@
+// autentificar, tanto funcionário quanto cliente
+export class SistemaAutenticacao{
+    static login(autenticavel, senha){
+        if(SistemaAutenticacao.ehAutenticavel(autenticavel)){
+            return autenticavel.autenticar(senha);
+        }
+        return false;
+    }
+
+    static ehAutenticavel(autenticavel){
+        return "autenticar" in autenticavel &&
+        autenticavel.autenticar instanceof Function;
+    }
+}
